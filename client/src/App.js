cd55070c649d82d81955e82a5fd783e4b9e64a73
Kenny Nguyen <alphakennyn@gmail.com>
2017-12-02 03:36:10 -0500

    V.1
    -Functional db
    -Functional display
    -Needs better format

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Location extends Component {
+    render() {
+      return (
+        <div className="thumbnail">
+          <img className="locationIcon" alt={this.props.Code} src="https://image.flaticon.com/teams/slug/freepik.jpg"/>
+          <p>{this.props.Code}</p>
+        </div>
+      );
+    }
+}
+
 class App extends Component {
-  state = {users: []}
+  state = {start: []}
 
   componentDidMount() {
-    fetch('/users')
-
-    .then(res => res.json())
-      .then(users => this.setState({ users }));
+    fetch('/trips').then(res => res.json())
+    .then(start => this.setState({ start }));
   }
+
   render() {
     return (
       <div className="App">
-        <h1>IT WORKS!!!</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <h1>JetBlue cheapy</h1>
+        <div className="leftCol">
+          {this.state.start.map(startObj =>
+            <Location Code={startObj.Code} />
+          )}
+        </div>
       </div>
     );
   }
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+  // And insert something like this instead:
   res.json([{
-    id: 1,
-    username: "samsepi0l"
+  	id: 1,
+  	username: "samsepi0l"
   }, {
-    id: 2,
-    username: "D0loresH4ze"
-  }]);  
+  	id: 2,
+  	username: "D0loresH4ze"
+  }]);
 });
 
 module.exports = router;
